4e24b05028d888c6e48bc24a2016c654bffa0cf4
”Pavlo <levkopavlo@gmail.com>
2024-05-30 00:48:06 +0300

    add AllArgsConstructor

--- src/main/java/com/example/demo/entity/Address.java
+++ src/main/java/com/example/demo/entity/Address.java
@@ -1,10 +1,11 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-
+@AllArgsConstructor
 public class Address {
     private String country;
     private String City;
