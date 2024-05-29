dd1367baf88e694588e7d2209f27cf6b15e9fd5b
”Pavlo <levkopavlo@gmail.com>
2024-05-30 00:46:22 +0300

    create first User

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.entity.Address;
+import com.example.demo.entity.Gender;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +20,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(UserRepository repository){
+		return args -> {
+			Address newYork = new Address("USA", "New-York", "234234234");
+			User userDoe = new User("Jon",
+					"Doe",
+					"jondoe@gmail.com",
+					List.of("books","computer"),
+					Gender.MALE,
+					newYork,
+					LocalDateTime.now()
+			);
+			repository.insert(userDoe);
+		};
+	}
 }
