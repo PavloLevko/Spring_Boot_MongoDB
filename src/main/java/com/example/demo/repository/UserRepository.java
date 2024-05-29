60e3707fe41171d80d29f46dadcb2f645130b181
”Pavlo <levkopavlo@gmail.com>
2024-05-30 01:25:28 +0300

    add method findUserByEmail

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
