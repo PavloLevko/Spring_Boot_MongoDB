f223b122e88d8fe10dcccbfded4e3c7e0fa7e8c1
”Pavlo <levkopavlo@gmail.com>
2024-05-31 14:25:04 +0300

    add method for create new User

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +15,9 @@ public class UserController {
     @GetMapping
 public List<User> getAllUser(){
     return userService.getAllUser();
+}
+@PostMapping
+public void addUser(@RequestBody User user) {
+userService.createUser(user);
 }
 }
